b6ab878be081810fa63b5331108232aca35c70af
Javier <javierenriqueos@gmail.com>
2022-12-01 23:49:37 -0500

    xd

--- app/src/main/java/com/example/shpiel/presentation/main/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/shpiel/presentation/main/viewmodels/MainViewModel.kt
@@ -1,16 +1,26 @@
 package com.example.shpiel.presentation.main.viewmodels
 
 import android.annotation.SuppressLint
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
+import com.example.shpiel.model.entity.User
+import com.example.shpiel.model.firebase.FirebaseManager
+import kotlinx.coroutines.launch
 
 class MainViewModel(
 
 ) : ViewModel(){
-    
      companion object {
          @SuppressLint("StaticFieldLeak")
          lateinit var navController: NavController
+         var usuario = mutableStateOf(User(nombre = "" , codigo = "" , alias = "" , id = ""))
          /*lateinit var user: String_*/
      }
+    fun getUser(id : String)  {
+        viewModelScope.launch {
+            FirebaseManager.instance.getUser(id) { usuario.value = it }!!
+        }
+    }
 }
